a6914636567bb235ce3c9afcb6fb4d822f546494
Vytas Kereisis <138277258+VytasKer@users.noreply.github.com>
2024-04-01 13:02:34 +0300

    Uploading initial files of the project

---
+++ Fotobankas/stockimages/src/test/java/com/registrationlogin/stockimages/StockimagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registrationlogin.stockimages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockimagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
