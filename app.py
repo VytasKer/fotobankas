68484f0947c91bb0c9b558aacf071f8416d3b141
VytasK <138277258+VytasKer@users.noreply.github.com>
2024-11-23 17:40:24 +0100

    Made changes on libraries installations

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import subprocess
+import sys
+
+# List of required packages
+required_packages = ["streamlit", "dropbox", "sqlite-utils"]
+
+# Install missing packages
+for package in required_packages:
+    try:
+        __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 pg = st.navigation([
     st.Page("main.py"),
