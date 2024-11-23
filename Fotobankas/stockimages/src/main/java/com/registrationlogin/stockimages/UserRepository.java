019d9af824261f4566f60fbda85d6dd79b9be1d3
VytasK <138277258+VytasKer@users.noreply.github.com>
2024-11-23 17:17:03 +0100

    Delete Fotobankas/stockimages directory
    Outdated

--- Fotobankas/stockimages/src/main/java/com/registrationlogin/stockimages/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.registrationlogin.stockimages;
- 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
- 
-}
